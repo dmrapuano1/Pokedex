40552e9073a88d0b275ff95e6b375c365501bd4f
dmrapuano1 <dmrapuano1@gmail.com>
2020-02-16 12:26:03 -0500

    replaced starting exercise with app code
    created a function constrcutor to create multiple pokemon with ease
    maintained console.log for easier visiblity while working on app, to be deleted later

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-alert('Hello World');
-var favoriteFood = 'pasta';
-document.write(favoriteFood);
+function Pokemon(name, height, ...types) {
+    this.name = name;
+    this.height = height;
+    this.types = [types];
+}
+
+var repository = [
+    new Pokemon('Bulbasaur', .7, 'grass', 'poison'),
+    new Pokemon('Charmander', .6, 'fire'),
+    new Pokemon('Squirtle', .5, 'water'),
+    ]
+
+console.log(repository)
