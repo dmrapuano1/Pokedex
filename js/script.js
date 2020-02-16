71fe83e1dd264c5f5fe9c474446ebc360be46afb
dmrapuano1 <dmrapuano1@gmail.com>
2020-02-16 13:15:32 -0500

    created a for loop to print information to the document
    for loop prints all pokemon names and heights to the document. In addition, for loop has an if/else to add text if pokemon is biger that 1m

--- js/script.js
+++ js/script.js
@@ -8,6 +8,15 @@ var repository = [
     new Pokemon('Bulbasaur', .7, 'grass', 'poison'),
     new Pokemon('Charmander', .6, 'fire'),
     new Pokemon('Squirtle', .5, 'water'),
-    ]
+    new Pokemon('Ninetales', 1.1, 'fire')
+    ];
 
-console.log(repository)
+for (i = 0; i < repository.length; i++) {
+    var heightText
+    if (repository[i].height >= 1) {
+        heightText = `(height:${repository[i].height} m) - Wow, that's big! `
+    } else {
+        heightText = `(height:${repository[i].height} m) `
+    }
+    document.write(`${repository[i].name} ${heightText} `)
+};
