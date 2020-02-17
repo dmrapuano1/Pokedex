3a7fb044b608a63c85f9c28994a03c52a71b902a
dmrapuano1 <dmrapuano1@gmail.com>
2020-02-17 13:49:30 -0500

    created IIFE  for code
    created IIFE for code overall and for the repository (now pokemonRepository). pokemonRepositiry returns add and getAll functions

--- js/script.js
+++ js/script.js
@@ -1,22 +1,57 @@
-function Pokemon(name, height, ...types) {
-    this.name = name;
-    this.height = height;
-    this.types = [types];
-}
+var myApplication = (function () {
 
-var repository = [
+    var pokemonRepository = (function () {
+
+        function Pokemon(name, height, ...types) {
+            this.name = name;
+            this.height = height;
+            this.types = [types];
+        }
+
+        var repository = [
     new Pokemon('Bulbasaur', 0.7, 'grass', 'poison'),
     new Pokemon('Charmander', 0.6, 'fire'),
     new Pokemon('Squirtle', 0.5, 'water'),
     new Pokemon('Ninetales', 1.1, 'fire'),
     ];
 
-repository.forEach(function (currentObject) {
-    var heightText;
-    if (currentObject.height >= 1) {
-        heightText = `(height:${currentObject.height} m) - Wow, that's big!`;
-    } else {
-        heightText = `(height:${currentObject.height} m)`;
-    };
-    document.write(`<p>${currentObject.name} ${heightText}</p>`);
-});
+        function add(name, height, ...types) {
+            if (typeof (name) !== 'string') {
+                alert(typeof name);
+                return false;
+            }
+            if (typeof (height) !== 'number') {
+                alert('Height must be a number');
+                return false;
+            }
+            for (i = 0; i < types.length; i++) {
+                if (typeof (types[i]) !== 'string') {
+                    alert(`All types must be a string. '${types[i]}' is not a string`);
+                    return false;
+                }
+            }
+            this.name = new Pokemon(name, height, ...types);
+            repository.push(this.name);
+        };
+
+        function getAll() {
+            return repository;
+        }
+        return {
+            add: add,
+            getAll: getAll
+        };
+    })();
+    pokemonRepository.add('Charzard', 2, 'fire', 'flying')
+    var list = pokemonRepository.getAll();
+    console.log(list);
+    list.forEach(function (currentObject) {
+        var heightText;
+        if (currentObject.height >= 1) {
+            heightText = `(height: ${currentObject.height}m) - Wow, that's big!`;
+        } else {
+            heightText = `(height: ${currentObject.height}m)`;
+        };
+        document.write(`<p>${currentObject.name} ${heightText}</p>`);
+    });
+})();
