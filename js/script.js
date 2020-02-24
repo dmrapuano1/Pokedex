9857b67a740660ed1fa14b0706426359af0e389e
dmrapuano1 <dmrapuano1@gmail.com>
2020-02-23 20:56:19 -0500

    updated to list items instead of div items

--- js/script.js
+++ js/script.js
@@ -113,10 +113,10 @@ var myApplication = (function () {
 
     var createPokemon = (function () { //Start of create Pokemon IIFE
         var addListItem = function (pokemon) {
-            var $itemButton = $(`<div class="col-sm-6 col-md-4 col-lg-3 form-group">
+            var $itemButton = $(`<li class="col-sm-6 col-md-4 col-lg-3 form-group">
                                     <div class="sr-only">Open information about pokemon: ${pokemon.name}</div>
                                     <button type="button" class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#modal-container">${pokemon.name}</button>
-                                </div>`);
+                                </li>`);
             eventFunction($itemButton, pokemon);
             $('.pokemon-list').append($itemButton);
         };
